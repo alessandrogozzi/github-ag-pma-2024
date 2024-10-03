d720d449f2c2fbf60fcc46637d032d376a9eb383
Alessandro Gozzi <alessandro.gozzi@tul.cz>
2024-10-03 15:40:17 +0200

    novy commit #3

--- MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
+++ MyApp004Objednavka/app/src/main/java/com/example/myapp004objednavka/MainActivity.kt
@@ -1,20 +1,60 @@
 package com.example.myapp004objednavka
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapp004objednavka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        enableEdgeToEdge()
+
+        //binding settings
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        title = "Objednávka kola"
+
+        binding.btnObjednavka.setOnClickListener {
+            val kolobezkaRbId = binding.rgKolobezky.checkedRadioButtonId
+
+            val kolobezka = findViewById<RadioButton>(kolobezkaRbId)
+
+            val vidlice = binding.cbVidlice.isChecked
+            val kola = binding.cbKola.isChecked
+            val vlakna = binding.cbVlakna.isChecked
+
+            val objednavkaText = "Souhrn objednávky: " + "${kolobezka.text}"
+
+            (if(vidlice) "; lepší vidlice" else "") +
+                    (if(kola) "; lepší kola" else "") +
+                    (if(vlakna) "; karbonová nanovlákna" else "")
+
+                    binding.tvObjednavka.text = objednavkaText
         }
+
+        //testovaci komentar ignoruj
+        //Změna obrázku v závislosti na vybraném radioButtonu
+        binding.kolobezka1.setOnClickListener {
+            binding.ivKolobezka.setImageResource(R.drawable.kolobezka1)
+        }
+
+        binding.kolobezka2.setOnClickListener {
+            binding.ivKolobezka.setImageResource(R.drawable.kolobezka2)
+        }
+
+        binding.kolobezka3.setOnClickListener {
+            binding.ivKolobezka.setImageResource(R.drawable.kolobezka3)
+        }
+
     }
 }
